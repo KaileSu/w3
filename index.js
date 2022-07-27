a8d960589aa69b50af908b4244272e72a3717872
Kaile Su <kailepku@gmail.com>
2022-07-27 20:14:25 +1000

    new Week3

--- index.js
+++ index.js
@@ -11,31 +11,31 @@ app.use(bodyParser.json());
 var http = require("http").Server(app);
 app.use(express.static(__dirname + '/www'));
 
-var server = http.listen(3000, function () {
+var server = http.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("My First Nodejs Server!");
-    console.log("Server listening on:"+ host + "port:" + port );
-  });
-  
-app.get('/test', function (req, res) {
+    console.log("Server listening on:" + host + "port:" + port);
+});
+
+app.get('/test', function(req, res) {
     res.sendFile(__dirname + '/www/myfirst.html');
 });
 
-app.post('/test1', function (req, res) {
+app.post('/test1', function(req, res) {
     var u = req.body.username;
     var p = req.body.psw;
-    c= u+":"+p;
+    c = u + ":" + p;
     console.log(c);
     fs.readFile('www/myfirst.html', 'utf8', function(err, data) {
         if (err) throw err;
         var pos = data.search(c);
-        var obj = {"ok":false,errors:{}};
-        var obj2 = {"ok":false,errors:{}};
+        var obj = { "ok": false, errors: {} };
+        var obj2 = { "ok": false, errors: {} };
         if (pos == -1) {
             console.log("no");
             res.send(JSON.stringify(obj));
-           // res.json( {"ok":false,errors:{}});
+            // res.json( {"ok":false,errors:{}});
         } else {
             res.send(JSON.stringify(obj2));
             //res.json({"ok":true});
@@ -45,20 +45,20 @@ app.post('/test1', function (req, res) {
 });
 
 
-app.post('/test2', function (req, res) {
+app.post('/test2', function(req, res) {
     var u = req.body.username;
     var p = req.body.psw;
-    c= u+":"+p;
+    c = u + ":" + p;
     console.log(c);
     fs.readFile('www/myfirst.html', 'utf8', function(err, data) {
         if (err) throw err;
         var pos = data.search(c);
-        var obj = {"ok":false,errors:{}};
-        var obj2 = {"ok":true};
+        var obj = { "ok": false, errors: {} };
+        var obj2 = { "ok": true };
         if (pos == -1) {
-           res.json(obj);
+            res.json(obj);
         } else {
-           res.json(obj2);
+            res.json(obj2);
         }
     });
 
